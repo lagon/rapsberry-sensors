9d778b5d91e8c15f28d3436a4cd8fc20212150a3
Miroslav Cepek <mcepek@vendavo.com>
2015-08-18 23:17:12 +0200

    GPS Initial version

---
+++ sensor_libraries/mtk3339-gps.h
@@ -0,0 +1,10 @@
+#ifndef __lagon_mtk3339_gps_h__
+#define __lagon_mtk3339_gps_h__
+
+#include "seriallib.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
