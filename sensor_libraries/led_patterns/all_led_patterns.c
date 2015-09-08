9b8e46ce61e096932da661fb811a5a57db4b4d4c
Miroslav Cepek <mcepek@vendavo.com>
2015-09-08 23:07:28 +0200

    LED patterns

--- sensor_libraries/led_patterns/all_led_patterns.c
+++ sensor_libraries/led_patterns/all_led_patterns.c
@@ -4,14 +4,14 @@
 #include <malloc.h>
 
 
-inline struct pa_LedStatesResults *allocateReturnStruct(int totalLeds) {
+struct pa_LedStatesResults *allocateReturnStruct(int totalLeds) {
 	struct pa_LedStatesResults *ret = (struct pa_LedStatesResults *) malloc(sizeof(struct pa_LedStatesResults));
 	ret->ledIntensities = (uint16_t *)malloc(sizeof(uint16_t) * totalLeds);
 	ret->totalLeds = totalLeds;
 	return ret;
 }
 
-inline void setIntensityToAllLeds(struct pa_LedStatesResults *ledStates, uint16_t intensity) {
+void setIntensityToAllLeds(struct pa_LedStatesResults *ledStates, uint16_t intensity) {
 	for (int ledID = 0; ledID < ledStates->totalLeds; ledID++) {
 		ledStates->ledIntensities[ledID] = intensity;
 	}
@@ -28,34 +28,42 @@ struct pa_LedStatesResults *ledPattern_setIntensityMediumFade(int time_step, int
 	const int max_time_step = 100;
 	struct pa_LedStatesResults * ret = allocateReturnStruct(totalLeds);
 	double step = ((double)(target_intensity - initial_intensity)) / ((double) max_time_step);
-	uint16_t intensity = (((uint16_t) step * time_step) + initial_intensity);
+	uint16_t intensity = (uint16_t)((step * time_step) + initial_intensity);
+
 	setIntensityToAllLeds(ret, intensity);
 	if (time_step >= max_time_step) {
 		ret->nextInvocation = pa_wasLastStep;
 	} else {
-		ret->nextInvocation = 500 * 1000;
+		ret->nextInvocation = 5 * 1000;
 	}
 	return ret;
 }
 
 struct pa_LedStatesResults *ledPattern_acknowledgeCommand(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity) {
 	struct pa_LedStatesResults * ret = allocateReturnStruct(totalLeds);
+	uint16_t intensity;
+	if (initial_intensity > 0x8000) {
+		intensity = initial_intensity - 0x4000;
+	} else {
+		intensity = initial_intensity + 0x4000;
+	}
+	
 	switch(time_step) {
 		case 0:
 			setIntensityToAllLeds(ret, initial_intensity);
 			ret->nextInvocation = 500 * 1000;
 			break;
 		case 1:
-			setIntensityToAllLeds(ret, target_intensity);
-			ret->nextInvocation = 500 * 1000;
+			setIntensityToAllLeds(ret, intensity);
+			ret->nextInvocation = 25 * 1000;
 			break;
 		case 2:
 			setIntensityToAllLeds(ret, initial_intensity);
-			ret->nextInvocation = 500 * 10000;
+			ret->nextInvocation = 500 * 1000;
 			break;
 		case 3:
-			setIntensityToAllLeds(ret, target_intensity);
-			ret->nextInvocation = 500 * 1000;
+			setIntensityToAllLeds(ret, intensity);
+			ret->nextInvocation = 25 * 1000;
 			break;
 		case 4:
 			setIntensityToAllLeds(ret, initial_intensity);
@@ -65,6 +73,37 @@ struct pa_LedStatesResults *ledPattern_acknowledgeCommand(int time_step, int tot
 	return ret;
 }
 
-// struct pa_LedStatesResults *ledPattern_darkSegmentSweep(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity) {
+struct pa_LedStatesResults *ledPattern_fiveMinuteDelay(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity) {
+	int max_time_step = 61;
+	struct pa_LedStatesResults * ret = allocateReturnStruct(totalLeds);
+	setIntensityToAllLeds(ret, initial_intensity);
+	int ledID = (time_step * totalLeds) / max_time_step;
 
-// }
+	if (time_step % 2 == 1) {
+		ret->nextInvocation = 10 * 1000 * 1000;
+		ret->ledIntensities[ledID] = initial_intensity;
+	} else {
+		ret->nextInvocation = 50 * 1000;
+		ret->ledIntensities[ledID] = target_intensity;
+	}
+
+	if (time_step >= max_time_step) {
+		ret->nextInvocation = pa_wasLastStep;
+	}
+
+	return ret;
+}
+
+struct pa_LedStatesResults *ledPattern_nightMode(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity) {
+	struct pa_LedStatesResults * ret = allocateReturnStruct(totalLeds);
+	setIntensityToAllLeds(ret, 0x0000);
+
+	ret->ledIntensities[0] = 0x1000;
+	ret->ledIntensities[1] = 0x0800;
+	ret->ledIntensities[2] = 0x0100;
+	ret->ledIntensities[3] = 0x0080;
+	ret->ledIntensities[4] = 0x0010;
+	ret->ledIntensities[5] = 0x0008;
+	ret->nextInvocation = pa_neverCallAgain;
+	return ret;
+}
--- sensor_libraries/led_patterns/all_led_patterns.h
+++ sensor_libraries/led_patterns/all_led_patterns.h
@@ -10,5 +10,7 @@ struct pa_LedStatesResults *ledPattern_setIntensityInOneStep(int time_step, int 
 struct pa_LedStatesResults *ledPattern_setIntensityMediumFade(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity);
 struct pa_LedStatesResults *ledPattern_acknowledgeCommand(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity);
 struct pa_LedStatesResults *ledPattern_darkSegmentSweep(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity);
+struct pa_LedStatesResults *ledPattern_fiveMinuteDelay(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity);
+struct pa_LedStatesResults *ledPattern_nightMode(int time_step, int totalLeds, uint16_t initial_intensity, uint16_t target_intensity);
 
 #endif
